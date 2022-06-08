3bb13e82fe45d87bde27da37f57dd2c034cfcf80
Gustavo Rocha <gustavorocha@Gustavos-Mac-mini.local>
2022-06-08 08:33:28 -0300

    adequação tela

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Service } from './../service/teste.service';
 import { CrawlerDTO } from './../models/models';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   templateUrl: './home.component.html',
@@ -9,15 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   movies: CrawlerDTO[] ;
+  cols: any[];
+  _selectedColumns: any[];
   constructor(private http: HttpClient,
     private service: Service) {}
 
   ngOnInit() {
+   
+    this.cols = [
+      { field: 'position', header: 'Position' },
+      { field: 'title', header: 'Title' },
+      { field: 'rating', header: 'Rating' },
+      { field: 'director', header: 'Rating' },
+      { field: 'casting', header: 'Casting' },
+      { field: 'comment', header: 'Comment' },
+  ];
     this.service.consultar(10).subscribe(retorno => {
       this.movies = retorno;
-      console.log(this.movies);
+      this._selectedColumns = this.cols;
     }, (erro: HttpErrorResponse) => {
     });
   }
+  @Input() get selectedColumns(): any[] {
+    return this._selectedColumns;
+}
 
+set selectedColumns(val: any[]) {
+    this._selectedColumns = this.cols.filter(col => val.includes(col));
+}
 }
